7cbea3ac3f60f892510951e5c74d33857263eb50
Kate Kozlova <ek@weare.lt>
2017-06-07 12:02:26 +0300

    START new branch for new code: REMOVE old-stuff, ADD new app code, UPD webpack-config entry point

--- frontend/home.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// модуль home, который подключает модуль welcome
-import welcome from './welcome';
-
-welcome("home");
-
-// экспортируем welcome "во вне"
-exports.welcome = welcome;
---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+var app =
+webpackJsonp_name_([0,1],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	document.getElementById('loginButton').onclick = function () {
+	  var login = __webpack_require__(1);
+
+	  login();
+	};
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+	'use strict';
+
+	module.exports = function () {
+	  alert("Log in!");
+	};
+
+/***/ })
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vYXBwLmpzIiwid2VicGFjazovLy8uL2xvZ2luLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIid1c2Ugc3RyaWN0JztcblxuZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ2xvZ2luQnV0dG9uJykub25jbGljayA9IGZ1bmN0aW9uKCkge1xuICBsZXQgbG9naW4gPSByZXF1aXJlKCcuL2xvZ2luJyk7XG5cbiAgbG9naW4oKTtcbn1cblxuXG5cbi8vIFdFQlBBQ0sgRk9PVEVSIC8vXG4vLyAuL2FwcC5qcyIsIid1c2Ugc3RyaWN0JztcblxubW9kdWxlLmV4cG9ydHMgPSBmdW5jdGlvbigpIHtcbiAgYWxlcnQoXCJMb2cgaW4hXCIpO1xufTtcblxuXG5cbi8vIFdFQlBBQ0sgRk9PVEVSIC8vXG4vLyAuL2xvZ2luLmpzIl0sIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBOzs7Ozs7QUNOQTtBQUNBO0FBQ0E7QUFDQTtBQUNBOzs7Iiwic291cmNlUm9vdCI6IiJ9
