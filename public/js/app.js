dfc8614c931df5ba620b24ba1bbece710161f4d9
Kate Kozlova <ek@weare.lt>
2017-06-07 14:22:20 +0300

    ADD "context", ADD "keys" in "app.js"

--- public/js/app.js
+++ public/js/app.js
@@ -52,35 +52,36 @@ var app =
 
 	// __ранее выражение внутри require было известно на этапе сборке, но так бывает не всегда
 	// __часто мы хотим, чтобы приложение получало URL и на основе него решало, какой модуль подгрузить
-	let route = __webpack_require__(1)("./" + moduleName);
-	route();
+
+	// __если этой функциональности по какой-то причине не хватает, то можно явно вызвать module context
+	// __из документации: require.context(directory, useSubdirectories = false, regExp = /^\.\//)
+	// __указывается 1) директория 2) флаг (true/false) - спускаться ли в поддиректории 3) regexp на который проверять
+
+	// __создадим контекст в этой директории,б не будем заходить в поддиректории, и регэксп нам в данном случае не нужен
+	// __добавим regexp, чтобы контекст "не путался" и загружал модули по одному разу
+	let context = __webpack_require__(6);
+
+	// __добавим keys
+	// __для каждого ключа из контекста будет прорекваерен соответствующий модуль и сразу вызван
+	context.keys().forEach(function(path) {
+	  let module = context(path);
+	  module();
+	});
+
+	// __добавим try-catch, чтобы отловить ошибки
+	// let route;
+	// try {
+	//   route = context('./' + moduleName);
+	// } catch (e) {
+	//   alert(e);
+	// }
+	// if (route) {
+	//   route();
+	// }
 
 
 /***/ }),
-/* 1 */
-/***/ (function(module, exports, __webpack_require__) {
-
-	var map = {
-		"./about": 2,
-		"./about.js": 2,
-		"./home": 3,
-		"./home.js": 3
-	};
-	function webpackContext(req) {
-		return __webpack_require__(webpackContextResolve(req));
-	};
-	function webpackContextResolve(req) {
-		return map[req] || (function() { throw new Error("Cannot find module '" + req + "'.") }());
-	};
-	webpackContext.keys = function webpackContextKeys() {
-		return Object.keys(map);
-	};
-	webpackContext.resolve = webpackContextResolve;
-	module.exports = webpackContext;
-	webpackContext.id = 1;
-
-
-/***/ }),
+/* 1 */,
 /* 2 */
 /***/ (function(module, exports) {
 
@@ -100,6 +101,30 @@ var app =
 	module.exports = function() {
 	  alert('home module');
 	}
+
+
+/***/ }),
+/* 4 */,
+/* 5 */,
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	var map = {
+		"./about.js": 2,
+		"./home.js": 3
+	};
+	function webpackContext(req) {
+		return __webpack_require__(webpackContextResolve(req));
+	};
+	function webpackContextResolve(req) {
+		return map[req] || (function() { throw new Error("Cannot find module '" + req + "'.") }());
+	};
+	webpackContext.keys = function webpackContextKeys() {
+		return Object.keys(map);
+	};
+	webpackContext.resolve = webpackContextResolve;
+	module.exports = webpackContext;
+	webpackContext.id = 6;
 
 
 /***/ })
