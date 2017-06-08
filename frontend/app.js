9ac5ae1774edd40d095dbf157c946c53d4c59224
Kate Sava <ek@weare.lt>
2017-06-08 12:24:41 +0300

    ADD new simple webpack-config with "externals", ADD CDN-library for example

--- frontend/app.js
+++ frontend/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// __допустим мы хотим использовать сторонний модуль moment, который мы установили в /node_modules
-// __в стороннем коде мы не можем руками разрулить проблемы с context
-let moment = require('moment');
+let _ = require('lodash');
 
-let today = moment(new Date()).locale('ja');
+let users = [
+  {id: "abcd", name: "Vasya"},
+  {id: "defa", name: "Petya"},
+  {id: "1234", name: "Masha"}
+];
 
-alert(today.format('DD MMM YYYY'));
-
-// __при этом наша сборка в public/app.js весит аж 510 Кб(!) и 118 модулей в придачу
+console.log( _pluck(users, 'name') );
